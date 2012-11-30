b0085839dc4e8a7f47a79105a04cda4832d511cb
Geoff Cagle <almost@technodookie.com>
2012-11-29 20:32:27 -0500

    Added WinMain. Not yet OpenGL / game ready.

---
+++ Wolf3D_Win32/WinMain.cpp
@@ -0,0 +1,150 @@
+#include <Windows.h>
+
+// Global Variables:
+static const TCHAR* szTitle			= TEXT( "Wolfenstein VR" );
+static const TCHAR* szWindowClass	= TEXT( "Wolfenstein VR Window" );
+
+// Forward declarations of functions included in this code module:
+static ATOM				MyRegisterClass(HINSTANCE hInstance);
+static BOOL				InitInstance(HINSTANCE, int);
+static LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+
+//
+//  FUNCTION: WinMain
+//
+//  PURPOSE: Init program and handle the main loop.
+//
+//  COMMENTS:
+//
+//    None.
+//
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+ 	// TODO: Place code here.
+	MSG msg = {0};
+
+	// Regester Windows Class.
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+//  COMMENTS:
+//
+//    This function and its usage are only necessary if you want this code
+//    to be compatible with Win32 systems prior to the 'RegisterClassEx'
+//    function that was added to Windows 95. It is important to call this function
+//    so that the application will get 'well formed' small icons associated
+//    with it.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex = {0};
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	//wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_NEWWINPROJ));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszClassName	= szWindowClass;
+	//wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND	- process the application menu
+//  WM_PAINT	- Paint the main window
+//  WM_DESTROY	- post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hWnd, &ps);
+
+			// TODO: Add any drawing code here...
+
+			EndPaint(hWnd, &ps);
+		}
+		break;
+
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+		}
+		break;
+
+		default:
+		{
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	}
+	return 0;
+}
